1411d803f0d17a21463cfd0e3691b1e379e9f993
Benjamin Abbott <bmacloud18@gmail.com>
2025-04-15 12:31:55 -0400

    debug

--- backend/src/main.py
+++ backend/src/main.py
@@ -19,7 +19,7 @@ DB_URL = os.environ.get('DB_URL')
 
 connection = psycopg.connect(
     host=os.environ.get('DB_HOST'),
-    database=os.environ.get('DB_NAME'),
+    dbname=os.environ.get('DB_NAME'),
     user=os.environ.get('DB_USER'),
     password=os.environ.get('DB_PASS'),
     port=os.environ.get('DB_PORT'),
