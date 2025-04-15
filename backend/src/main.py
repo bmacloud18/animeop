b61190811621ded2337f33afbbd5a2411fcac962
Benjamin Abbott <bmacloud18@gmail.com>
2025-04-15 12:42:01 -0400

    kinda worked, adding insert statements

--- backend/src/main.py
+++ backend/src/main.py
@@ -93,6 +93,19 @@ def db_test():
                     vid_url TEXT NOT NULL
                 );
             """)
+            db.execute("""
+                INSERT INTO videos (vid_title, vid_url) VALUES 
+                    ('Fairy Tail Opening 1', 'https://www.youtube.com/watch?v=9jvVBVcZ0-Y'), 
+                    ('Dragon Ball Z Opening', 'https://www.youtube.com/watch?v=R4vjJrGeh1c'),
+                    ('One Piece Opening 20', 'https://www.youtube.com/watch?v=Oo52vQyAR6w'),
+                    ('Demon Slayer Opening 1', 'https://www.youtube.com/watch?v=YkJvHe3KK2c'),
+                    ('Sword Art Online Opening 1', 'https://www.youtube.com/watch?v=1oOBjyOKu2o'),
+                    ('Tokyo Ghoul Opening 1', 'https://www.youtube.com/watch?v=7aMOurgDB-o'),
+                    ('Death Note Opening 1', 'https://www.youtube.com/watch?v=kNyR46eHDxE'),
+                    ('My Hero Academia Opening 1', 'https://www.youtube.com/watch?v=yu0HjPzFYnY'),
+                    ('Fullmetal Alchemist: Brotherhood Opening 1', 'https://www.youtube.com/watch?v=elyXcwunIYA'),
+                    ('Attack on Titan Opening 1', 'https://www.youtube.com/watch?v=8OkpRK2_gVs');
+            """)
             db.execute("SET search_path TO public")
             db.execute('SELECT * FROM videos')
             return (['db test:', DB_URL] + db.fetchall())
