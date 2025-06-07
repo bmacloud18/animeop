f84930794a1d20cf8bdf6d44350e5aa1f20fd776
Benjamin Abbott <bmacloud18@gmail.com>
2025-06-07 12:58:39 -0400

    refined yt search query in attempt to ensure official video plays, not lyric videos. also corrected shiny hunter link

--- reactf/app/page.tsx
+++ reactf/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
       </div>
       <div className="flex flex-col h-fit gap-12">
         <Redirect text="Anime OP/ED Theme Channel" location="/animeop"></Redirect>
-        <Redirect text="Shiny Hunter" location="http://ec2-18-222-208-193.us-east-2.compute.amazonaws.com"></Redirect>
+        <Redirect text="Shiny Hunter" location="http://ec2-3-16-129-81.us-east-2.compute.amazonaws.com"></Redirect>
         <Redirect text="Good Calc" location="https://goodcalc.vercel.app/"></Redirect>
         <Redirect text="RNG Lotto Wheel" location="https://lottery-nine-tau.vercel.app/"></Redirect>
       </div>
